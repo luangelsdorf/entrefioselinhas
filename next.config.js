4ff198d0811ab22fc46681a1aa0817bfca3dae26
luangelsdorf <luanferreira2136@gmail.com>
2021-12-06 13:48:08 -0300

    Added template files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
+      },
+    ]
+  },
 }
